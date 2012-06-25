ca864c73106da54d53d6c9af14f567b6ccc5c9d3
Motiejus Jakštys <desired.mta@gmail.com>
2012-06-25 15:16:40 +0200

    More verbose error message on port open failure

--- src/lua_driver.erl
+++ src/lua_driver.erl
@@ -5,19 +5,19 @@
 -include("lua.hrl").
 
 open() ->
-  {ok, L} = load_driver(),
-  #lua{port=L}.
-  
+    {ok, L} = load_driver(),
+    #lua{port=L}.
+
 close(#lua{port=Port}) ->
-  port_close(Port).
+    port_close(Port).
 
 
 %% Private functions
 load_driver() ->
-  SearchDir = filename:join([filename:dirname(code:which(lua_driver)), "..", "priv"]),
-  case erl_ddll:load(SearchDir, liberlua) of
-    ok ->
-      {ok, open_port({spawn, 'liberlua'}, [binary])};
-    Error ->
-      Error
-  end.
+    SearchDir = filename:join([filename:dirname(code:which(lua_driver)), "..", "priv"]),
+    case erl_ddll:load(SearchDir, liberlua) of
+        ok ->
+            {ok, open_port({spawn, 'liberlua'}, [binary])};
+        {error, Error} ->
+            {error, erl_ddll:format_error(Error)}
+    end.
