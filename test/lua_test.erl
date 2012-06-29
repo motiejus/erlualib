1087e10e3d72b89dced714ca1a474618aefffe8b
Motiejus Jakštys <desired.mta@gmail.com>
2012-06-30 01:47:12 +0200

    Tests take less space

--- test/lua_test.erl
+++ test/lua_test.erl
@@ -2,41 +2,17 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-small_integer_test() ->
-    push_to_helper(1, pushinteger, tointeger).
-    
-zero_integer_test() ->
-    push_to_helper(0, pushinteger, tointeger).
-    
-small_negative_integer_test() ->
-    push_to_helper(-2, pushinteger, tointeger).
-    
-
-small_number_test() ->
-    push_to_helper(2, pushnumber, tonumber).
-    
-small_negative_number_test() ->
-    push_to_helper(-2, pushnumber, tonumber).
-    
-zero_number_test() ->
-    push_to_helper(0, pushnumber, tonumber).
-
-big_number_test() ->
-    push_to_helper(5000000000, pushnumber, tonumber).
-    
-big_floating_number_test() ->
-    push_to_helper(5000000000.234, pushnumber, tonumber).
-    
-big_negative_number_test() ->
-    push_to_helper(-5000000000, pushnumber, tonumber).
-    
-big_negative_float_number_test() ->
-    push_to_helper(-5000000000.234, pushnumber, tonumber).
-
-
-string_test() ->
-    push_to_helper("testing", pushstring, tolstring).
-
+small_integer_test() -> push_to_helper(1, pushinteger, tointeger).
+zero_integer_test() -> push_to_helper(0, pushinteger, tointeger).
+small_negative_integer_test() -> push_to_helper(-2, pushinteger, tointeger).
+small_number_test() -> push_to_helper(2, pushnumber, tonumber).
+small_negative_number_test() -> push_to_helper(-2, pushnumber, tonumber).
+zero_number_test() -> push_to_helper(0, pushnumber, tonumber).
+big_number_test() -> push_to_helper(5000000000, pushnumber, tonumber).
+big_float_number_test() -> push_to_helper(5000000000.234, pushnumber, tonumber).
+big_neg_number_test() -> push_to_helper(-5000000000, pushnumber, tonumber).
+big_neg_float_test() -> push_to_helper(-5000000000.234, pushnumber, tonumber).
+string_test() -> push_to_helper("testing", pushstring, tolstring).
 
 call_test() ->
     {ok, L} = lua:new_state(),
@@ -51,7 +27,6 @@ set_get_global_test() ->
     ?assertMatch(ok, lua:setfield(L, global, "foo")),
     ?assertMatch(ok, lua:getfield(L, global, "foo")),
     ?assertMatch({ok, 23}, lua:tonumber(L, 1)).
-
 
 push_to_helper(Val, Push, To) ->
     {ok, L} = lua:new_state(),
