feb441828a52b5962341e8c8ff7271d218bc88f7
Motiejus Jakštys <desired.mta@gmail.com>
2012-07-11 19:37:54 +0200

    Add tests and documentation for experiments
    Calling Lua functions and returning values with arbitrarily nested
    values

--- src/lual.erl
+++ src/lual.erl
@@ -1,9 +1,61 @@
+%%% @doc LuaL, extended function calling
+%%%
+%%% This module is responsible for high-level function invocations in Lua.
+%%%
+%%% ## Erlang->Lua type conversion table
+%%%     >-------->------>
+%%% +------------+----------------+
+%%% | Erlang     |      Lua       |
+%%% +------------+----------------+
+%%% | binary     | string         |
+%%% | atom       | string         |
+%%% | string     | string         |
+%%% | number     | number         |
+%%% | proplist   | tagged table   |
+%%% | tuple      | indexed table  |
+%%% +------------+----------------+
+%%%
+%%% Whereas >--------->----->
+%%% +-----------------+------------+
+%%% |       Lua       |   Erlang   |
+%%% +-----------------+------------+
+%%% | nil             | nil        |
+%%% | boolean         | true|false |
+%%% | light_user_data | --NA--     |
+%%% | number          | float      |
+%%% | string          | binary     |
+%%% | table           | proplist   |
+%%% | function        | --NA--     |
+%%% | user_data       | --NA--     |
+%%% | thread.         | --NA--     |
+%%% +-----------------+------------+
+%%%
+%%% ## Table to proplist conversion rules
+%%% Table key conversion rules are same like for any other data type.
+%%%
+%%% ## Function return value handling
+%%% When function returns with one value, give it plain.
+%%% When function returns more values, wrap them to tuple.
+
 -module(lual).
 
--export([dostring/2]).
+-export([dostring/2, call/4]).
 
 -include("lua.hrl").
 -include("lua_api.hrl").
+
+-type arg() :: binary() | % string
+               atom()   | % string
+               number() | % number
+               list()   | % table
+               tuple().   % table
+-type args() :: [arg(), ...].
+
+-type ret() :: nil       |
+               boolean() |
+               float()   |
+               binary()  |
+               list({ret(), ret()}).
 
 dostring(#lua{port=Port}, Code) ->
     port_command(Port, term_to_binary({?ERL_LUAL_DOSTRING, Code})),
@@ -22,3 +74,7 @@ recieve_simple_response() ->
     after ?STD_TIMEOUT ->
         {error, timeout}
     end.
+
+-spec call(lua:lua(), string(), args(), pos_integer()) -> ret().
+call(L, Fun, Args, Num) ->
+    ok.
---
+++ test/lual_test.erl
@@ -0,0 +1,29 @@
+-module(lual_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+number_test() -> sah(9).
+float_test() -> sah(9.21).
+neg_float_test() -> sah(-9.21).
+binary_test() -> sah(<<"hiho">>).
+atom_test() -> sah(hiho, <<"hiho">>).
+
+proplist1_test() -> sah([{1, 1}]).
+proplist2_test() -> sah([{1,1}, {2, 2}]).
+proplist3a_test() -> sah([{1,1},{2,<<"x">>}]).
+proplist3b_test() -> sah([{1,1},{2,'x'}], [{1,1},{2,<<"x">>}]).
+
+tuple1_test() -> sah({1}, [{1, 1}]).
+tuple2_test() -> sah({1,2}, [{1,1}, {1,2}]).
+tuple3a_test() -> sah({1,<<"x">>}, [{1,1},{2,<<"x">>}]).
+tuple3b_test() -> sah({1,'x'}, [{1,1},{2,<<"x">>}]).
+
+
+%% @doc Single Arg Helper
+sah(Val) ->
+    sah(Val, Val).
+sah(Val, Expect) ->
+    {ok, L} = lua:new_state(),
+    lual:dostring(L, <<"function t(c) return c end">>),
+    ?assertEqual(Val, lual:call(L, "t", [Val], 1)),
+    lua:close(L).
