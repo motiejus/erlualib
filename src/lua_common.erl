ce6512e29b92a1d4dbc40457d277604e43a73156
root <root@li528-100.members.linode.com>
2013-01-19 13:51:09 +0000

    specified lua return message

--- src/lua_common.erl
+++ src/lua_common.erl
@@ -9,10 +9,10 @@ command(#lua{port=Port}, Data) ->
 
 receive_valued_response() ->
     receive
-        ok -> ok;
-        {ok, Str} -> Str;
-        {throw, Throw} -> throw(Throw);
-        Other -> throw({unknown_return, Other})
+        lua_ok -> ok;
+        {lua_ok, Str} -> Str;
+        {lua_throw, Throw} -> throw(Throw)
+%        Other -> throw({unknown_return, Other})
     after ?STD_TIMEOUT ->
             erlang:error(timeout)
     end.
